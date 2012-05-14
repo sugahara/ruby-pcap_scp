8818ef7c0a59c956980bed625d13216375464a8c
Jun Sugahara <j.sghr.jp@gmail.com>
2012-05-14 13:44:59 +0900

    add pcap_file_count() method, modify bug which writing pcap file may be moved

--- ruby-dumpscp.rb
+++ ruby-dumpscp.rb
@@ -1,6 +1,19 @@
+# -*- coding: undecided -*-
 require 'net/ssh'
 require 'net/scp'
 require 'fileutils'
+
+#現在のpcapファイルの数をかぞえるメソッド
+def pcap_file_cont(files)
+  count = 0
+  files.each do |name|
+    if File.extname(name)==".pcap"
+      count+=1
+    end
+  end
+  count
+end
+
 
 if ARGV[0][ARGV[0].size-1] == "/"
   local_dir = ARGV[0].chop
@@ -16,17 +29,19 @@ options = {
 }
 files = Dir::entries(local_dir)
 
-
-
-files.each do |name|
-  if File.extname(name) == ".pcap"
-    new_dir_name = name.split("_")[2][0...8]
-      if new_dir_name == Time.now.strftime("%Y%m%d")
+if pcap_file_count(files) > 1
+  latest_files = Dir::entries(local_dir)
+  files.each do |name|
+    if File.extname(name) == ".pcap"
+      new_dir_name = name.split("_")[2][0...8]
+      if new_dir_name == Time.now.strftime("%Y%m%d") || pcap_file_count(latest_files) <= 1
         #DO NOTHING(A FILE IN USE)
       else
         Dir::mkdir("#{local_dir}/#{new_dir_name}", 0776) unless FileTest.exist?("#{local_dir}/#{new_dir_name}")
         File.rename("#{local_dir}/#{name}", "#{local_dir}/#{new_dir_name}/#{name}")
+        latest_files = Dir::entries(local_dir)
       end
+    end
   end
 end
 puts "dir END"
